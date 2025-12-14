d9acf7982f122f987f7f37d3c3c09ad5c5fed03f
Noorbilu <nuralabonno@gmail.com>
2025-12-15 01:41:46 +0600

    Added more feature

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,13 @@ import Banner from './Banner';
 import Divider from './Divider';
 import HowItWorks from './HowItWorks';
 import TestimonialsSection from './TestimonialsSection';
+import QuickSteps from './QuickSteps';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <QuickSteps></QuickSteps>
             <Divider></Divider>
             <HowItWorks></HowItWorks>
             <TestimonialsSection></TestimonialsSection>
